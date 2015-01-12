b0ac4b52a7bd5d5acd5b32aa6f5bf6256c76b86a
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-12 16:30:49 -0500

    Initial commit, add CSV with cfe data, create school model

---
+++ db/migrate/20150112212957_create_schools.rb
@@ -0,0 +1,12 @@
+class CreateSchools < ActiveRecord::Migration
+  def change
+    create_table :schools do |t|
+      t.string :dbn
+      t.string :school
+      t.integer :total_enrollment
+      t.integer :amount_owed
+
+      t.timestamps
+    end
+  end
+end
