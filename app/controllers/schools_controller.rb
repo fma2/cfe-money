09a8977f7cce51c47f930a7391dea53ec1894d7e
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-14 19:35:05 -0500

    Change results varialbe to use searchscope method, add show method - not using yet though

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -4,4 +4,13 @@ class SchoolsController < ApplicationController
 		
 	end
 	
+	def search
+		# @results = PgSearch.multisearch(params["search"])
+		@results = School.search_schools(params["search"])
+	end
+
+	def show
+		#AJAX to get info on single
+	end
+
 end
