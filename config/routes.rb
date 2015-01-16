8a73156c90c2f839d20cef46ba03cbe9e78b1edc
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-16 14:34:01 -0500

    Add route for ajax call for getting 50 random schools

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'schools#index'
   get '/search' => 'schools#search'
+  get '/random' => 'schools#random40'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
