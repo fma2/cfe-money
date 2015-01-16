431ce37e822080bac41d6c98f282eacc226f98fc
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-16 14:25:45 -0500

    Add ajax call to get 50 random schools as json; add method to place list of schools in background

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -1,21 +1,50 @@
 $( document ).ready(function() {
-	console.log("ready!");
+  $("body").hide(0).delay(100).fadeIn(2200);
+  
+  $.ajax({
+    dataType: 'json',
+    url: '/random',
+    type: 'GET'
+  }).success(function(data) {
+    createDynamicTextBackground(data)
+  }).fail(function() {
+    console.log("Failed to load background text")
+  });
+  
 });
 
 $(window).on('show.bs.modal', function (event) {
-	console.log("hi");
   var button = $(event.relatedTarget) // Button that triggered the modal
   var dbn = button.data('dbn'); // Extract info from data-* attributes
   var school = button.data('school');
   var enrollment = button.data('enrollment');
   var amount = button.data('owed');
-  console.log(enrollment);
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $('#schoolModal')
   modal.find('.modal-title').text(school);
   modal.find('.amount-number').text("$" + amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'));
   modal.find('.enrollment-number').text(enrollment);
-  // modal.find('.modal-title').text('New message to ' + recipient)
-  // modal.find('.modal-body input').val(recipient)
 });
+
+
+function createDynamicTextBackground(data) {
+  console.log(data);
+  var txt = [];
+  for (k in data) {
+    txt.push(data[k].dbn);
+    txt.push(data[k].school);
+    txt.push(data[k].total_enrollment);
+    txt.push("$" + data[k].amount_owed.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'))
+  }
+  var txt_now = '';
+  for (var i = 0; i < txt.length; i++) {
+    txt_now += txt[i] + ' ';
+  }
+  var txt_rep = '', l = 0;
+  while(l < 100) {
+    txt_rep += txt_now;
+    l++;
+  }    
+  var attribute = document.createAttribute("data-bg");
+  attribute.value = txt_rep;
+  document.body.setAttributeNode(attribute);
+}
