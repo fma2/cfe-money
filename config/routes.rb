8cd148703600ed8d65c85147c930a31fdb97b849
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-16 19:52:43 -0500

    Add route to get districts info for ajax call

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'schools#index'
   get '/search' => 'schools#search'
   get '/random' => 'schools#random40'
+  get '/districts' => 'schools#districts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
