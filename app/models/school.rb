85e3477e9637cc1908d747bb41965d72f5590bf9
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-26 10:14:09 -0500

    Add ability to do partial text search in school.rb

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,9 @@
 class School < ActiveRecord::Base
 	include PgSearch
 	multisearchable :against => [:dbn, :school]
-	pg_search_scope :search_schools, :against => [:dbn, :school];
+	pg_search_scope :search_schools, 
+									:against => [:dbn, :school], 
+									:using => {
+                    :tsearch => {:prefix => true}
+                  }
 end
