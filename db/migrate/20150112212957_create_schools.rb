73f680d4e0cd9de1de53d7e26aa75849bf50aae3
Farheen Malik <farheen.m.malik@gmail.com>
2015-02-17 23:07:16 -0500

    Update migration file for schools to add additional fields for new data

--- db/migrate/20150112212957_create_schools.rb
+++ db/migrate/20150112212957_create_schools.rb
@@ -1,10 +1,14 @@
 class CreateSchools < ActiveRecord::Migration
   def change
     create_table :schools do |t|
-      t.string :dbn
+    	t.string :dbn
       t.string :school
       t.integer :total_enrollment
       t.float :amount_owed
+      t.string :district_name
+      t.integer :district_no
+      t.string :assembly_district
+      t.integer :senate_district
 
       t.timestamps
     end
