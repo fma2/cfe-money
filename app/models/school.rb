e807e80a4f38eb547d0c63cd7253562ba8b3c180
Farheen Malik <farheen.m.malik@gmail.com>
2015-02-18 10:41:27 -0500

    Add methods to get districts_num_arr, get_district_code, get_district_name, get list of district schools, get district enrollment sum, get district owed sum. :

--- app/models/school.rb
+++ app/models/school.rb
@@ -5,4 +5,36 @@ class School < ActiveRecord::Base
 									:using => {
                     :tsearch => {:prefix => true},
                   }
+  	
+  def self.districts_num_arr
+  	districts = School.select(:district_no).map(&:district_no).uniq
+  	districts.delete_if { |d| d == nil }
+  	districts.sort
+  end
+
+  def self.get_district_code(schools_arr)
+  	district_code = schools_arr.map(&:district_code).uniq
+  	district_code[0]
+  end
+
+  def self.get_district_name(schools_arr)
+  	district_name = schools_arr.map(&:district_name).uniq
+  	district_name[0]
+  end
+
+  def self.district_schools(num)
+		School.where(district_no: num)
+	end
+
+	def self.district_enrollment_sum(schools_arr)
+		schools_arr.sum(:total_enrollment)
+	end
+
+	def self.district_owed_sum(schools_arr)
+		schools_arr.sum(:amount_owed)
+	end
+
+	def self.ad_schools(num)
+	end
+
 end
