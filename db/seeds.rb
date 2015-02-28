a554584d6514256951c12cf2a4896c12bd41513e
fma2 <farheen.m.malik@gmail.com>
2015-02-28 17:39:16 -0500

    Add code to seed location table with two endpoints from location json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,29 @@
 require 'csv' 
 require 'faraday'
 
+Location.delete_all
 School.delete_all   
 
-location = Location.first
-url = URI(location.endpoint)
-connection = Faraday.new(url: url.to_s)
+# Seed Locations
+loc_url = URI("https://cfe-data.herokuapp.com/locations")
+connection = Faraday.new(url: loc_url.to_s)
 response = connection.get
 collection = JSON.parse(response.body)
-
 collection.each do |item|
-	location.schools << School.create!(item)
+	Location.create!(item)
+end
+
+
+# Seed Schools
+locations = Location.all
+locations.each do |l|
+	location = l
+	schools_url = URI(location.endpoint)
+	connection = Faraday.new(url: schools_url.to_s)
+	response = connection.get
+	collection = JSON.parse(response.body)
+
+	collection.each do |item|
+		location.schools << School.create!(item)
+	end
 end
