1d6dcf6a0dd5c009fd3a59dee180da5cd16b5f98
fma2 <farheen.m.malik@gmail.com>
2015-03-01 11:11:08 -0500

    Fix calculation issue with nyc totals

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,8 +15,8 @@ class LocationsController < ApplicationController
 	def nyc_index
 		@location = Location.find_by(loc_code:"nyc")
 		@location_name = @location.name
-		@total_owed = School.total_owed_sum(@location.schools) / 2
-		@total_enrollment = School.total_enrollment_sum(@location.schools) / 2
+		@total_owed = School.total_owed_sum(@location.schools)
+		@total_enrollment = School.total_enrollment_sum(@location.schools)
 		@amount_per_student = School.amount_per_student(@total_owed, @total_enrollment)
 	end
 end
