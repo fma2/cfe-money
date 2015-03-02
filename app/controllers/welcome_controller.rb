d548915c9fed7c99a4ee8d9a9c90f3ea6d8f0143
fma2 <farheen.m.malik@gmail.com>
2015-03-02 14:25:20 -0500

    Add instance variable to index method to be used in view - a bit of a monkeypatch again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,20 @@
 class WelcomeController < ApplicationController
 	def index
 		@locations = Location.all
+		@totals = Array.new
+		Location.all.each do |l |
+			total_owed = School.total_owed_sum(l.schools)
+			total_enrollment = School.total_enrollment_sum(l.schools)
+			@totals << {
+								l.loc_code => {
+										id: l.id,
+										location_name: l.name,
+										total_owed: total_owed,
+										total_enrollment: total_enrollment,
+										amount_per_student: School.amount_per_student(total_owed, total_enrollment)
+								} 
+								}	
+		end
+		@totals
 	end
 end
