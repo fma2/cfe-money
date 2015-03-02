3f238302474a948e6676977c1dc0ee8408520794
fma2 <farheen.m.malik@gmail.com>
2015-03-01 21:50:18 -0500

    Comment out dynamic text js

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  $("body").hide(0).delay(100).fadeIn(2200);
+  // $("body").hide(0).delay(100).fadeIn(2200);
 
   //AJAX call for dynamic text background data
   $.ajax({
@@ -69,6 +69,27 @@ $(document).on('show.bs.modal', function (event) {
 });
 
 
+// function createDynamicTextBackground(data) {
+//   var txt = [];
+//   for (k in data) {
+//     txt.push(data[k].school);
+//     txt.push(data[k].total_enrollment);
+//     txt.push("$" + data[k].amount_owed.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"))
+//   }
+//   var txt_now = '';
+//   for (var i = 0; i < txt.length; i++) {
+//     txt_now += txt[i] + ' ';
+//   }
+//   var txt_rep = '', l = 0;
+//   while(l < 100) {
+//     txt_rep += txt_now;
+//     l++;
+//   }    
+//   var attribute = document.createAttribute("data-bg");
+//   attribute.value = txt_rep;
+//   document.body.setAttributeNode(attribute);
+// }
+
 function createDynamicTextBackground(data) {
   var txt = [];
   for (k in data) {
@@ -84,10 +105,11 @@ function createDynamicTextBackground(data) {
   while(l < 100) {
     txt_rep += txt_now;
     l++;
-  }    
+  }
+  var title = document.getElementById('background')    
   var attribute = document.createAttribute("data-bg");
   attribute.value = txt_rep;
-  document.body.setAttributeNode(attribute);
+  title.setAttributeNode(attribute);
 }
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
