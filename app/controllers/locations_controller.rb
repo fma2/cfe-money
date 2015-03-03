4a4fad487677e08f30a2670b907c8e8085002a25
fma2 <farheen.m.malik@gmail.com>
2015-03-02 22:06:18 -0500

    Remove methods for locations_controller - not using

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,22 +1,2 @@
 class LocationsController < ApplicationController
-
-	def show
-
-	end
-
-	def ros_index
-		@location = Location.find_by(loc_code:"ros")
-		@location_name = @location.name
-		@total_owed = School.total_owed_sum(@location.schools)
-		@total_enrollment = School.total_enrollment_sum(@location.schools)
-		@amount_per_student = School.amount_per_student(@total_owed, @total_enrollment)
-	end
-
-	def nyc_index
-		@location = Location.find_by(loc_code:"nyc")
-		@location_name = @location.name
-		@total_owed = School.total_owed_sum(@location.schools)
-		@total_enrollment = School.total_enrollment_sum(@location.schools)
-		@amount_per_student = School.amount_per_student(@total_owed, @total_enrollment)
-	end
 end
