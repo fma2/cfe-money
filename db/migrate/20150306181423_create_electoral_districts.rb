6b11afc00b1d8936112aafb04e3123c63908cb75
fma2 <farheen.m.malik@gmail.com>
2015-03-07 17:15:26 -0500

    Add fields to electoral districts table

--- db/migrate/20150306181423_create_electoral_districts.rb
+++ db/migrate/20150306181423_create_electoral_districts.rb
@@ -2,14 +2,17 @@ class CreateElectoralDistricts < ActiveRecord::Migration
   def change
     create_table :electoral_districts do |t|
     	t.string :photo
+    	t.string :first_name
+    	t.string :last_name
     	t.string :full_name
     	t.string :email
     	t.string :house
     	t.string :district_no
     	t.string :district_name
     	t.string :website
-    	t.string :number
-    	
+    	t.string :albany_office_no
+    	t.string :do_office_no
+
       t.timestamps
     end
   end
