60aece22bf0b4583345ac0e68e3eef6e555f5ab2
fma2 <farheen.m.malik@gmail.com>
2015-03-07 17:16:24 -0500

    modify route for search to use loc_code not an id number

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  match '/search/:id' => 'schools#search', via: [:get], as: "search"
+  match '/search/:loc_code' => 'schools#search', via: [:get], as: "search"
   get '/random' => 'schools#random40'
   get '/school_districts' => 'schools#districts'
   get '/electoral_districts' => 'schools#electoral_districts'
