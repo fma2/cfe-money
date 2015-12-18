6f507c3bd6eacd07966f23e85f6e14d7cbd6c2fd
fma2 <farheen.m.malik@gmail.com>
2015-12-17 22:40:30 -0500

    Delete electoral district model

--- app/models/electoral_district.rb
+++
@@ -1,5 +0,0 @@
-class ElectoralDistrict < ActiveRecord::Base
-	has_many :electoral_district_schools
-	has_many :schools, through: :electoral_district_schools
-
-end
--- app/models/electoral_district_school.rb
+++
@@ -1,6 +0,0 @@
-class ElectoralDistrictSchool < ActiveRecord::Base
-	
-	belongs_to :school
-	belongs_to :electoral_district
-
-end
