98fe94737edd1953798839eb2275bbd61fcd83c4
fma2 <farheen.m.malik@gmail.com>
2015-12-20 15:28:19 -0500

    Add route for each school's page

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,17 @@ Rails.application.routes.draw do
   # pages routes
   root 'welcome#index'
   get '/districts' => 'districts#index'
+  get '/schools/:location/:school_id' => 'schools#show'
 
   # routes for ajax calls
   get '/school_districts' => 'districts#school_districts'
   get '/electoral_districts/:leg_chamber/:leg_district' => 'districts#electoral_districts'
+
   # school search route
   match '/search/:loc_code' => 'schools#search', via: [:get], as: "search" 
 
   # misc
   # get '/random' => 'schools#random40'
+
   
  end
