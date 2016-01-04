e94c57e07f2eabf9e9e41ac259637caeaaf8e324
fma2 <farheen.m.malik@gmail.com>
2016-01-04 17:35:13 -0500

    Got AJAX to work with bitly to produce shortened url

--- app/controllers/bitly_controller.rb
+++ app/controllers/bitly_controller.rb
@@ -1,15 +1,7 @@
 class BitlyController < ApplicationController
 	def new
-		# u = Bitly.client.shorten(params["url"])
-		u = Bitly.client.shorten("http://cfemoneyowednys.org")
-		p u
-		p u.short_url
-		# p u
-		# if !(u)
-		# 	u = Bitly.client.shorten("http://cfemoneyowednys.org")
-		# 	return @url = u.short_url
-		# end
-		@json = u.short_url
+		@json = Array.new
+		@json << Bitly.client.shorten(params["url"]).short_url
 		respond_to do |format|
 			format.html
 			format.json { render json: @json }
