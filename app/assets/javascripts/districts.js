667432ad47c645b5f8a2aaa2f7ae8775160c8e76
fma2 <farheen.m.malik@gmail.com>
2016-01-08 14:26:30 -0500

    Update layout of electoral district information

--- app/assets/javascripts/districts.js
+++ app/assets/javascripts/districts.js
@@ -127,7 +127,7 @@ function createElectedOfficialInformation(data,infodiv) {
     if (data.chamber == "upper") {
       chamber = "SD"
     } else if  (data.chamber == "lower") {
-      chamber = "AD0"
+      chamber = "AD"
     }
     x.append(
       '<div class="accordion-group">' +
@@ -136,33 +136,22 @@ function createElectedOfficialInformation(data,infodiv) {
             '<button type="button" class="btn btn-primary btn-block"><i class="fa fa-angle-right"></i> '+chamber+''+data.district+' - '+data.last_name+'</button>' +
           '</a>' +
         '</div>' +
+
         '<div id="'+data.chamber+''+data.district+'" class="accordion-body collapse">' +
           '<div class="accordion-inner">' +
             '<div class="row">'+
-              '<div class="col-md-7">' +
-                '<h5 class="well-title">total amount owed to '+data.chamber+' '+data.district+'</h5>'+
-                '<h3 class="well-content highlight-title"></h3>' +
-              '</div>'+
-              '<div class="col-md-5">' +
-                '<h5 class="well-title">total number students</h5>' +
-                '<h3 class="well-content highlight-title"></h3>' +
-              '</div>'+
-            '</div>'+
-            '<div class="row">'+
-              '<div class="col-md-7">' +
-                '<h5 class="well-title well-schools-list-title">schools in district</h5>'+
-                "<div class='well-schools-list'></div>" +
-              '</div>'+
-              '<div class="col-md-5">' +
+              '<div class="col-md-6">' +
                 "<div class='well-legislator-information'>" +
-                  '<h5 class="well-title">elected official</h5>'+
-                  "<div class='col-lg-3'>" +
-                    "<div class='well-leg-photo'><a href="+data.url+" target='_blank'><img class='img-responsive' src='"+data.photo_url+"'></a></div>" +
+                  '<h5 class="well-title">legislator</h5>'+
+                  "<div class='col-lg-5'>" +
+                    "<div class='well-leg-photo'><a href="+data.url+" target='_blank'><img class='img-responsive img-rounded' src='"+data.photo_url+"'></a></div>" +
                   "</div>" +
-                  "<div class='col-lg-9'>" +
+                  "<div class='col-lg-7'>" +
                     "<h4 class='well-leg-name'><a href="+data.url+" target='_blank'>"+data.full_name+"</a></h4>" +
-                    "<p class='well-leg-house'>"+data.chamber+"</p>" +
-                    "<h5 class='well-leg-district'>"+data.district+"</h5>" +
+                    "<h5>" +
+                    "<span class='well-leg-house'>"+chamber+"</span> " +
+                    "<span class='well-leg-district'>"+data.district+"</span>" +
+                    "</h5>" +
                     "<p class='well-leg-email'><a href='mailto:"+data.email+"?Subject=What are you doing to get CFE money back to our schools?'>"+data.email+"</a></p>" +
                     "<p class='well-leg-albanyno'><a href='tel:"+data.offices[0]["phone"]+"'>"+data.offices[0]["phone"]+"</a></p>" +
                     "<p class='well-leg-website'>" +
@@ -171,6 +160,25 @@ function createElectedOfficialInformation(data,infodiv) {
                   "</div>" +
                   "<br>" +
                 "</div>" +
+              '</div>'+
+              "<div class='col-md-6'>" +
+
+              "</div>" +
+            '</div>'+
+            '<div class="row">'+
+              '<div class="col-md-7">' +
+                '<h5 class="well-title">total amount owed to district</h5>'+
+                '<h3 class="well-content highlight-title"></h3>' +
+              '</div>'+
+              '<div class="col-md-5">' +
+                '<h5 class="well-title">total number students</h5>' +
+                '<h3 class="well-content highlight-title"></h3>' +
+              '</div>'+
+            '</div>'+
+            '<div class="row">'+
+              '<div class="col-md-12">' +
+                '<h5 class="well-title well-schools-list-title">schools in district</h5>'+
+                "<div class='well-schools-list'></div>" +
               '</div>'+
             '</div>'+
           '</div>' +
